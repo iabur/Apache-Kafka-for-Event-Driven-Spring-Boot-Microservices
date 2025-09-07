a02f7a41206361b16155c4f120da0a9a3356ada3
iabur <rahman.iabur@technonext.com>
2025-09-07 18:20:31 +0600

    add idempotent producer settings to Kafka configuration for improved reliability

--- src/main/java/com/iabur/kafkademo/config/KafkaConfig.java
+++ src/main/java/com/iabur/kafkademo/config/KafkaConfig.java
@@ -33,8 +33,15 @@ public class KafkaConfig {
     @Value("${spring.kafka.producer.properties.linger.ms}")
     private String lingerMs;
 
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private String enableIdempotence;
+
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private String maxInFlightRequestsPerConnection;
+
     @Bean
     public NewTopic topic() {
+
         return TopicBuilder.name("product-created-event-topic")
                 .partitions(3)
                 .replicas(3)
@@ -52,6 +59,10 @@ public class KafkaConfig {
         props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
         props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMs);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequestsPerConnection);
+        // comment it out because use deliveryTimeout above
+//        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
         return props;
     }
 
